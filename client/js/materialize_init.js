58d7bb67b06d95301cf0a9c912bc759529338eac
Kevin Zhang <kevin.zhang.13499@gmail.com>
2015-07-08 21:19:45 -0700

    Add materialize init to allow certain jquery effects

---
+++ client/js/materialize_init.js
@@ -0,0 +1,22 @@
+// dialogs
+$('.modal-trigger').leanModal();
+
+$('.tooltipped').tooltip({"delay": 50});
+
+// tabs
+$('ul.tabs').tabs();
+
+// media
+$('.materialboxed').materialbox();
+
+// collapsible
+$('.collapsible').collapsible();
+
+// mobile collapse button
+$('.button-collapse').sideNav();
+
+// dropdowns
+$('.dropdown-button').dropdown();
+
+// select forms
+$('select').material_select();
